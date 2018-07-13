a769e8a8efda0bf438ffd4ce4bba373c0ddc38f8
inshener34 <inshener34@mail.ru>
2018-07-13 13:31:11 +0500

    1.3.1 Maven Plugins

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -3,7 +3,7 @@ package ru.job4j.calculate;
 /** class Calculate
 */
 public class Calculate {
-        /**  метод main
+        /**  metod main
         */
         public static void main(String[] args) {
               System.out.println("Hello World");

7bcd6ed8fef46a02871f8d3f6322b60cdb8a776a
inshener34 <inshener34@mail.ru>
2018-07-13 13:15:32 +0500

    1.3. JavDoc. Code convetion.

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,7 +1,10 @@
 package ru.job4j.calculate;
 
+/** class Calculate
+*/
 public class Calculate {
-     
+        /**  метод main
+        */
         public static void main(String[] args) {
               System.out.println("Hello World");
         }

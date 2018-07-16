24ba79acbfd328059d8c60ca6aeb552a22f98890
inshener34 <inshener34@mail.ru>
2018-07-16 14:07:48 +0500

    2.2. Импорт Maven проекта.

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -20,7 +20,7 @@ public class Calculate {
          * @param name Your name.
          * @return Echo plus your name.
         */
-       public String echo (String name) {
+       public String echo(String name) {
               return "Echo, echo, echo : " + name;
        }
 }

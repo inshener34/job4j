e80e3616306b1066fcea07a6a6cceb11ca97c73e
inshener34 <inshener34@mail.ru>
2018-07-18 16:43:40 +0500

    3.4 Расстояние между точками в системе координат.

--- chapter_001/src/main/java/ru/job4j/condition/package-info.java
+++ chapter_001/src/main/java/ru/job4j/condition/package-info.java
@@ -1,7 +1,7 @@
 /**
-* Package for calculate task
+* Package for condition task
 * @author Alexandr Mukonin (inshener34@mail.ru)
 * @version $ld$
 * @since 0.1
 */
-package ru.job4j.calculate;
+package ru.job4j.condition;
--- chapter_001/src/test/java/ru/job4j/Calculator/CalculatorTest.java
+++ chapter_001/src/test/java/ru/job4j/Calculator/CalculatorTest.java
@@ -8,7 +8,7 @@ import static org.junit.Assert.assertThat;
 public class CalculatorTest {
     @Test
     public void whenAddOnePlusOneThenTwo() {
-        Calculator calc = new Calculator();
+        Сalculator calc = new Сalculator();
         calc.add(1D, 1D);
         double result = calc.getResult();
         double expected = 2D;

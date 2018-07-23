5762bd998a70da23a78470bd5f8920b003e58eec
inshener34 <inshener34@mail.ru>
2018-07-23 15:44:05 +0500

    4.2. Максимум из двух чисел.2

--- chapter_001/src/main/java/ru/job4j/max/Max.java
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -2,7 +2,7 @@ package ru.job4j.max;
 
 public class Max {
     public int max(int first, int second) {
-        int max = first >= second ? first : second;
-        return max;
+        int maxim = first > second ? first : second;
+        return maxim;
     }
 }

614acbbeb92950432312b33c5160ab51ad0ffa88
inshener34 <inshener34@mail.ru>
2018-11-02 18:03:55 +0500

    Заново

--- chapter_001/src/main/java/ru/job4j/dop/Dop1.java
+++ chapter_001/src/main/java/ru/job4j/dop/Dop1.java
@@ -2,7 +2,7 @@ package ru.job4j.dop;
 
 public class Dop1 {
     public static boolean isSorted(int[] a, boolean array) {
-        if (array == false) {
+        if (!array) {
             for (int i = 0; i < a.length - 1; ++i) {
                 if (a[i] > a[i + 1]) {
                     return false;
